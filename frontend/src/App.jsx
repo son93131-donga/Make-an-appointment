e41bf70989e3ef193b620e7da6d1d119d250f99a
son93131-donga <sondoanqn123@gmail.com>
2025-04-26 11:42:49 +0700

    Them cac trang page & dinh tuyen route & them assetss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div className='mx-4 sm:mx-[10%]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
 export default App
+
