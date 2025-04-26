753d3721a52365803359c8d12d1f804a5a99dbe0
son93131-donga <sondoanqn123@gmail.com>
2025-04-26 19:37:47 +0700

    them header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
+import Doctors from './pages/Doctors'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
     <Router>
       <div className='mx-4 sm:mx-[10%]'>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/doctors' element={<Doctors />} />
+          <Route path='/doctors/:speciality' element={<Doctors />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/my-appointments' element={<MyAppointments />} />
+          <Route path='/appointments/:docId' element={<Appointment />} />
+          <Route path='/my-profile' element={<MyProfile />} /> 
+        
         </Routes>
       </div>
     </Router>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      homepage
+   
     </div>
   )
 }
 
-export default Home
+export default Home;
