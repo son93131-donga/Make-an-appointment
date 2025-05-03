0ca3ab70dbfeaf815b2aac33f65800b7a04d70da
son93131-donga <sondoanqn123@gmail.com>
2025-05-03 16:32:04 +0700

    them cac link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,25 +7,25 @@ const Navbar = () => {
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img className='w-44 cursor-pointer ' src={assets.logo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-medium'>
-        <NavLink>
+        <NavLink to='/' >
             <li className='py-1' >HOME</li>
             <hr  className='border-none outline-none h-0.5 bg-primary w-3/5 auto'/>
         </NavLink>
-        <NavLink>
+        <NavLink  to='/doctors'>
             <li className='py-1'>ALL DOCTOR</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 auto'/>
         </NavLink>
-        <NavLink>
+        <NavLink to='/about'>
             <li className='py-1'>ABOUT</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 auto'/>
         </NavLink>
-        <NavLink>
+        <NavLink to='/contact'>
             <li className='py-1'>CONTACT</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 auto'/>
         </NavLink>
       </ul>
       <div>
-        <button>Create account</button>
+        <button className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
       </div>
     </div>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors:{
+//         'primary':'#5f6FFF'
+//       } 
+//     },
+//   },
+//   plugins: [],
+// }
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    'bg-primary',
+    'text-white',
+    'rounded-full',
+    'px-8',
+    'py-3',
+    'font-light',
+    'md:block',
+    'hidden'
   ],
   theme: {
     extend: {
