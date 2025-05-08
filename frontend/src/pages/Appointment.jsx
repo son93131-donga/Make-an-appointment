7295927ef4e8c213bcf96f3df13cd939ce8bbb2e
son93131-donga <sondoanqn123@gmail.com>
2025-05-08 21:51:23 +0700

    related doc

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets';
+import RelatedDoctors from '../components/RelatedDoctors';
 
 
 const Appointment = () => {
@@ -126,14 +127,27 @@ const Appointment = () => {
       <div className='flex gap-3 items-center w-full mt-4 overflow-x-scroll'> 
         {
           docSlots.length && docSlots.map((item, index)=> (
-            <div className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'}`  } key={index}>
+            <div onClick={()=> setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'}`  } key={index}>
               <p>{item[0] && dayOfWeek[item[0].datetime.getDay()]}</p>
               <p>{item[0] && item[0].datetime.getDate()}</p>
             </div>
           )) 
         }
       </div>
+
+      <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+        {docSlots.length && docSlots[slotIndex].map((item, index) =>(
+          <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white ' : 'text-gray-400 border border-gray-300'}`} key={index} >
+            {item.time.toLowerCase()}
+          </p>
+        ) )}
+      </div>
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
        </div>
+
+       {/* list related doctoc */}
+       <RelatedDoctors docId={docId} speciality={docInfo.speciality}  />
+
     </div>
   )
 }
