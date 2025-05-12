e3bfb1cf5464ca1fff43dd92076d87f1d4957c59
son93131-donga <sondoanqn123@gmail.com>
2025-05-12 22:14:51 +0700

    aadmin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator"
 import bcrypt from 'bcrypt'
 import { v2 as cloudinary } from "cloudinary"
 import doctorModel from "../models/doctorModel.js"
+import jwt from 'jsonwebtoken'
 
 
 //API for add doctor
@@ -64,6 +65,16 @@ const addDoctor = async (req, res) => {
 const loginAdmin = async (req, res)=>{
     try {
         
+        const {email, password} = req.body
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            
+            const token = jwt.sign(email+password, process.env.JWT_SECRET )
+            res.json({success: true, token})
+            
+        } else {
+            res.json({success: false, message:"Invalid credentials"})
+        }
+
     } catch (error) {
         console.log(error)
         res.json({success: false, message:error.message})
